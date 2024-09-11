008a8671309ce1c58f26609a8c70e12d8e73022f
Abdul Muhsin K <muhsinamk111@gmail.com>
2024-09-11 18:19:04 +0530

    Lecture #4

--- wp-content/themes/aquila/header.php
+++ wp-content/themes/aquila/header.php
@@ -7,12 +7,13 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aquila Wordpress Theme</title>
+    <?php wp_head(); ?>
 </head>
 
 <body>
